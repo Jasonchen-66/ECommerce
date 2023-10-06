dbc4d51846788ed1b3ce6afc6429379f2672eebf
Jianbang Chen <77318357+Jasonchen-66@users.noreply.github.com>
2023-10-05 22:08:25 -0400

    upload
    upload file to github

---
+++ djackets_django/product/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+
+from product import views
+
+urlpatterns = [
+    path('latest-products/',views.LatestProductsList.as_view()),
+    path('products/search/',views.search),
+    path('products/<slug:category_slug>/<slug:product_slug>/',views.ProductsDetials.as_view()),
+    path('products/<slug:category_slug>/',views.CategoryDetial.as_view()),
+]
+
+
